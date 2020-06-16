5859d0866d10d8983d7b9eeb4e543ae21ac8da85
Utkarsh-cedcoss <saxenautkarsh172@gmail.com>
2020-06-16 19:21:25 +0530

    add option to display time

--- wp-content/plugins/hello-world/wp-plugin_hello.php
+++ wp-content/plugins/hello-world/wp-plugin_hello.php
@@ -12,3 +12,23 @@ Version: 1.7.2
 Author URI: http://ma.tt/
 */
 
+
+function add_my_menu(){
+    add_menu_page(
+    "helloworld",
+    "HelloWorld",
+    "manage_options",
+    "hello-world",
+    "add_function",
+    "dashicons-buddicons-replies",
+    );
+    }
+    add_action("admin_menu","add_my_menu");
+    
+    function add_function(){
+    echo "here we will display time";
+    $date=date("h:i:sa");
+    echo $date;
+    }
+
+
